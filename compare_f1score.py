149e8c863f6710a9c596656f673240525bd488f8
EstherMaria <esthervdenberg@gmail.com>
2016-09-04 13:51:59 +0200

    changes to conv check

--- compare_f1score.py
+++ compare_f1score.py
@@ -5,14 +5,16 @@ def read_op(file):
     tmp = [o.strip(';').strip('\n') for o in tmp.split(' ') if o != '']
     return tmp[-1]
 
-def compare(files):
+def conv_check(files):
     tmps = [float(read_op(file)) for file in files]
     if abs(tmps[0] - tmps[1]) < 0.01:
-        print('Converged')
+        return True
+    else:
+        return False
 
 with open('state.txt', 'w') as f:
     f.write('')
 
-if compare(['conlleval_before_one_it', 'conlleval_after_one_it']) == True:
+if conv_check(['conlleval_before_one_it', 'conlleval_after_one_it']) == True:
     with open('state.txt', 'w') as f:
         f.write('Converged')
